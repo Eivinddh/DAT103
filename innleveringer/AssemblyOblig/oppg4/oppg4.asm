5ab704d343436945b01d5e213bbb0d410753f09e
eivinddh <eivind.d.halderaker@hotmail.com>
2017-10-25 12:37:36 +0200

    It's working!

--- innleveringer/AssemblyOblig/oppg4/oppg4.asm
+++ innleveringer/AssemblyOblig/oppg4/oppg4.asm
@@ -31,60 +31,42 @@ _start:
   jmp MyLoop
   
 MyLoop:
-  push edx
-  push ecx
   cmp ecx,10
-  ja Sub ;conditioned/unconditioned?
   jl Add
+
+  Sub:
+    dec edx
+    jmp RestLoop
   
 Add:
-  push edx
   INC edx
-  pop edx
-  jmp RestLoop
-  
-Sub:
-  push edx
-  DEC edx
-  pop edx
   jmp RestLoop
  
 RestLoop:
-  push edx
-  push ecx
   inc ecx
   cmp ecx,20
   je Slutt
   jmp MyLoop 
-
-Slutt:
-  push edx
-  push ebx
-  push eax
-  add edx,'0'
-  mov [siffer],ecx
-  mov ecx,siffer
-  mov edx,1
-  mov ebx,STDOUT
-  mov eax,SYS_WRITE
-  int 80h
-  mov eax,SYS_EXIT
-  mov ebx,0
-  int 80h
   
 nylinje:
-  push eax
-  push ebx
-  push ecx
-  push edx
   mov edx,crlflen
   mov ecx,crlf
   mov ebx,STDOUT
   mov eax,SYS_WRITE
   int 80h
-  pop edx
-  pop ecx
-  pop ebx
-  pop eax
   ret
+
+Slutt:
+  mov ebx,STDOUT
+  mov eax,SYS_WRITE
+  add edx,'0'
+  mov [siffer], edx
+  mov ecx,siffer
+  mov edx,1
+  
+  int 80h
+  call nylinje
+  mov eax,SYS_EXIT
+  mov ebx,0
+  int 80h
 ; End _start
